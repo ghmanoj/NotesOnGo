3343d5558914e6bac415431f567cd291c02365f5
Manoj <ermanojghimire@gmail.com>
2021-08-06 20:52:08 -0700

    Add app icon and commentout things not used

--- NotesOnGo/Features/Settings/SettingsView.swift
+++ NotesOnGo/Features/Settings/SettingsView.swift
@@ -12,58 +12,59 @@ struct SettingsView: View {
 	@AppStorage("isDarkMode") var isDarkMode: Bool = true
 	
 	@ObservedObject private var viewModel = ObjectUtils.settingsViewModel
-	
-	@State private var isDevCommand: Bool = false
-	
-	@State private var seperator = "\n"
-	
-	private let availableDevCommands = [
-		"Command: utility, Modifier: logout",
-		"Command: utility, Modifier: lock",
-		"Command: system, Modifier: status",
-	]
+//
+//	@State private var isDevCommand: Bool = false
+//
+//	@State private var seperator = "\n"
+//
+//	private let availableDevCommands = [
+//		"Command: utility, Modifier: logout",
+//		"Command: utility, Modifier: lock",
+//		"Command: system, Modifier: status",
+//	]
 	
 	var body: some View {
 		VStack(alignment: .leading, spacing: 20) {
 			
 			Toggle("Dark Mode", isOn: $isDarkMode)
 				.font(.title2)
-			
-			Toggle("Dev. Cmds", isOn: $isDevCommand)
-				.font(.title2)
-
-			Text(isDevCommand ? availableDevCommands.joined(separator: seperator) : "")
-				.frame(minHeight: 20)
-				.foregroundColor(.red.opacity(0.9))
-				.font(.footnote)
+//
+//			Toggle("Dev. Cmds", isOn: $isDevCommand)
+//				.font(.title2)
+//
+//			Text(isDevCommand ? availableDevCommands.joined(separator: seperator) : "")
+//				.frame(minHeight: 20)
+//				.foregroundColor(.red.opacity(0.9))
+//				.font(.footnote)
 			
 			Spacer(minLength: 0)
 
-			VStack(alignment: .leading, spacing: 10) {
-				HStack(spacing: 10) {
-					Text("Api End Point")
-						.font(.caption)
-						.foregroundColor(.secondary)
-					
-					TextField(viewModel.currentApiEndPoint, text: $viewModel.apiEndPoint)
-						.padding(7)
-						.background(Color.secondary.opacity(0.15))
-						.cornerRadius(7)
-					
-					Button(action: {
-						viewModel.onSetApiEndPoint()
-					}) {
-						Text("Set")
-					}
-				}
-				Text(viewModel.notice)
-					.foregroundColor(.red.opacity(0.5))
-					.font(.footnote)
-					.frame(height: 25)
-			}
-			.onAppear {
-				viewModel.fetchLatest()
-			}
+//			VStack(alignment: .leading, spacing: 10) {
+//				HStack(spacing: 10) {
+//					Text("Api End Point")
+//						.font(.caption)
+//						.foregroundColor(.secondary)
+//
+//					TextField(viewModel.currentApiEndPoint, text: $viewModel.apiEndPoint)
+//						.padding(7)
+//						.background(Color.secondary.opacity(0.15))
+//						.cornerRadius(7)
+//
+//					Button(action: {
+//						viewModel.onSetApiEndPoint()
+//					}) {
+//						Text("Set")
+//					}
+//				}
+//				Text(viewModel.notice)
+//					.foregroundColor(.red.opacity(0.5))
+//					.font(.footnote)
+//					.frame(height: 25)
+//			}
+//			.onAppear {
+//				viewModel.fetchLatest()
+//			}
+			
 		}
 		.padding()
 		.frame(maxHeight: .infinity)
